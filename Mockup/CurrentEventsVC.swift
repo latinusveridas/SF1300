0a1f6bdaf4c5093717c2776e104f0fa3cb59fb75
John <sparteclaudius749@gmail.com>
2018-12-16 21:22:55 +0100

    Large improvment

--- Mockup/CurrentEventsVC.swift
+++ Mockup/CurrentEventsVC.swift
@@ -13,18 +13,52 @@ import AlamofireImage
 
 class CurrentEventsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    // ============================== VARIABLES
+    // ============================== VARIABLES ===============================
+    @IBOutlet weak var tableView: UITableView!
     
     // When data is received, it's filled in eventsList
     var eventsList: [eventClass] = []
     
+    // Loading of the ViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+
+        
+        let SFTokenHandler = StreetFitTokenHandler()
+        let sessionManager = SFTokenHandler.sessionManager
+        sessionManager.adapter = SFTokenHandler
+        sessionManager.retrier = SFTokenHandler
+        let urlString = "http://83.217.132.102:3000/auth/experlogin/innerjoin"
+        populateEventsList(targetURL: urlString, theSessionManager: sessionManager) { eventsList in
+            guard eventsList != nil else {return}
+            self.eventsList = eventsList!
+            self.tableView.reloadData()
+        }
+        
+        
+        
+        
+        
+        
+        
     }
     
     
 // ============================== TABLE FUNCTIONS ============================
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        //return UITableView.automaticDimension
+        return 115
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 115
+    }
+    
     
     // Count received events in eventsList
 
@@ -35,9 +69,9 @@ class CurrentEventsVC: UIViewController, UITableViewDelegate, UITableViewDataSou
     // Populate data in cells
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "protoEventCell", for: indexPath) as! eventUICell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath) as! eventUICell
         //cell.labelEvent_id.text = eventsList[indexPath.row].event_id
-        cell.labelDate.text = eventsList[indexPath.row].date
+        //cell.labelDate.text = eventsList[indexPath.row].date
         cell.labelLocation.text = eventsList[indexPath.row].location
         cell.labelSport.text = eventsList[indexPath.row].sport
         cell.labelFirstName.text = eventsList[indexPath.row].first_name
@@ -48,6 +82,13 @@ class CurrentEventsVC: UIViewController, UITableViewDelegate, UITableViewDataSou
         cell.labelSubscribed.text = "\(eventsList[indexPath.row].nb_part_sub)"
         cell.labelPart_max.text = "\(eventsList[indexPath.row].nb_part_max)"
         cell.labelPrice.text = "\(eventsList[indexPath.row].price_per_part)"
+        
+        cell.UIImage_Sport.image = UIImage(named: "yoga")
+        
+        var intPartMax = Float(eventsList[indexPath.row].nb_part_max)
+        var intSub = Float(eventsList[indexPath.row].nb_part_sub)
+        
+        cell.ParticipationProgressBar.progress = intSub/intPartMax
         
         // Last Step : Image download through AlamofireImage
         if let organizerID = eventsList[indexPath.row].organizer_id as? String {
@@ -99,14 +140,18 @@ class CurrentEventsVC: UIViewController, UITableViewDelegate, UITableViewDataSou
 class eventUICell: UITableViewCell {
     
     //@IBOutlet weak var labelEvent_id: UILabel!
-    @IBOutlet weak var labelDate: UILabel!
+    //@IBOutlet weak var labelDate: UILabel!
     @IBOutlet weak var labelLocation: UILabel!
     @IBOutlet weak var labelSport: UILabel!
     @IBOutlet weak var labelSubscribed: UILabel!
     @IBOutlet weak var labelPart_max: UILabel!
     @IBOutlet weak var labelPrice: UILabel!
     @IBOutlet weak var UIImage_OPP: UIImageView!
+    @IBOutlet weak var UIImage_Sport: UIImageView!
     @IBOutlet weak var labelFirstName: UILabel!
+    
+    @IBOutlet weak var ParticipationProgressBar: UIProgressView!
+    
     //@IBOutlet weak var labelLatitude: UILabel!
     //@IBOutlet weak var labelLongitude: UILabel!
     
---
+++ Mockup/StreetFitTokenHandler.swift
@@ -0,0 +1,158 @@
+//
+//  StreetFitTokenHandler.swift
+//  Mockup
+//
+//  Created by Quentin Duquesne on 16/12/2018.
+//  Copyright © 2018 Quentin Duquesne. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class StreetFitTokenHandler: RequestAdapter, RequestRetrier {
+    
+    // Definition du RefreshCompletion utilisé lors de la requete RefreshToken
+    private typealias RefreshCompletion = (_ succeeded: Bool, _ jwt1: String?, _ jwt2: String?) -> ()
+    
+    
+    // Definition du SessionManager
+    let sessionManager: SessionManager = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
+        
+        return SessionManager(configuration: configuration)
+        
+    } ()
+    
+    
+    // Autres variables
+    private let lock = NSLock()
+    
+    private var jwt1: String?
+    private var jwt2: String?
+    private var baseURLString: String
+    
+    private var isRefreshing = false
+    private var requestsToRetry: [RequestRetryCompletion] = []
+    
+    // Initialization
+    
+    public init() {
+        self.jwt1 = UserDefaults.standard.string(forKey: "jwt1")
+        self.jwt2 = UserDefaults.standard.string(forKey: "jwt2")
+        self.baseURLString = "http://83.217.132.102:3000/"
+    }
+    
+    
+    // Request Adapter
+    func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
+        if let urlString = urlRequest.url?.absoluteString, urlString.hasPrefix(baseURLString) {
+            var urlRequest = urlRequest
+            print("DEBUG ADAPTER JWT1 IS", jwt1!)
+            urlRequest.setValue(jwt1, forHTTPHeaderField: "jwt1")
+            urlRequest.setValue(jwt2, forHTTPHeaderField: "jwt2")
+            return urlRequest
+        }
+        
+        return urlRequest
+    }
+    
+    // RequestRetrier
+    func should(_ manager: SessionManager, retry request: Request, with error: Error, completion: @escaping RequestRetryCompletion) {
+        if let response = request.task?.response as? HTTPURLResponse, response.statusCode == 401 {
+            requestsToRetry.append(completion)
+            
+            if !isRefreshing {
+                print("Refresh is require, launching of the refreshToken function")
+                refreshToken { [weak self] succeeded, jwt1,jwt2 in
+                    guard let strongSelf = self else {return}
+                    
+                    strongSelf.lock.lock() ; defer {strongSelf.lock.unlock()}
+                    
+                    if let jwt1 = jwt1, let jwt2 = jwt2 {
+                        strongSelf.jwt1 = jwt1
+                        strongSelf.jwt2 = jwt2
+                        
+                    }
+                    
+                    strongSelf.requestsToRetry.forEach { $0(succeeded, 0.0) }
+                    strongSelf.requestsToRetry.removeAll()
+                    
+                }
+            }
+        } else {
+            completion(false,0.0)
+        }
+    }
+    
+    //RefreshToken
+    private func refreshToken (completion: @escaping RefreshCompletion) {
+        
+        let urlString = "http://83.217.132.102:3000/auth/refresh"
+        guard !isRefreshing else { return }
+        
+        isRefreshing = true
+        
+        let headers: HTTPHeaders = [
+            "jwt1" : jwt1!
+        ]
+        
+        sessionManager.request(urlString, method: .post,encoding: JSONEncoding.default, headers: headers).responseJSON { [weak self] response in
+            guard let strongSelf = self else { return }
+            
+            let json = response.result.value as! [String:Any]
+            let data = json["data"] as! [String:Any]
+            let jwt2 = data["JWT2"] as? String
+            
+            if
+                let json = response.result.value as? [String:Any],
+                let data = json["data"] as? [String:Any],
+                let jwt2 = data["JWT2"] as? String
+                
+            {
+                UserDefaults.standard.set(jwt2, forKey: "jwt2")
+                completion(true,self?.jwt1,jwt2)
+            } else {
+                completion(false,nil,nil)
+            }
+            
+            strongSelf.isRefreshing = false
+            
+        }
+    }
+    
+    
+}
+
+// ================================== MAIN FUNCTIONS ====================================
+
+func populateEventsList (targetURL: String,theSessionManager: SessionManager, completion: @escaping ([eventClass]?) -> Void) {
+
+    
+    theSessionManager.request(targetURL, method: .get)
+        .validate()
+        .responseJSON { response in
+            
+            //print(response)
+            
+            switch response.result {
+                
+            case .success:
+                
+                guard response.result.isSuccess else {return completion(nil)}
+                guard let rawInventory = response.result.value as? [[String:Any]?] else {return completion(nil)}
+                
+                let inventory = rawInventory.compactMap { EvenDict -> eventClass? in
+                    let data = EvenDict!
+                    return eventClass(data: data)
+                }
+                completion(inventory)
+                
+            case .failure(let error):
+                print(error)
+                
+            }
+            
+    }
+    
+}
