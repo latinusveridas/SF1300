a8892a1cb7f066969bd91beef184e3fa2aa768f0
John <sparteclaudius749@gmail.com>
2018-12-18 21:56:28 +0100

    Create Event

---
+++ Mockup/EventCreation.swift
@@ -0,0 +1,25 @@
+//
+//  EventCreation.swift
+//  Mockup
+//
+//  Created by Quentin Duquesne on 18/12/2018.
+//  Copyright © 2018 Quentin Duquesne. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateEventVC: UIViewController {
+    
+    @IBOutlet weak var DatePicked: UILabel!
+    @IBOutlet weak var Adress: UITextField!
+    @IBOutlet weak var sportselected: UILabel!
+    @IBOutlet weak var priceField: UITextField!
+    @IBOutlet weak var ParticipantField: UITextField!
+    @IBOutlet weak var CreateEventButton: UIButton!
+    @IBOutlet weak var DatePicker: UIDatePicker!
+    @IBOutlet weak var SportPicker: UIPickerView!
+    
+
+}
+
