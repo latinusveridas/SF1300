fbfd93af1de651b9ca3b7651d02ceb6dcf879696
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2018-12-19 10:02:38 +0100

    Update EventCreation.swift

--- Mockup/EventCreation.swift
+++ Mockup/EventCreation.swift
@@ -20,6 +20,12 @@ class CreateEventVC: UIViewController {
     @IBOutlet weak var DatePicker: UIDatePicker!
     @IBOutlet weak var SportPicker: UIPickerView!
     
+        override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        // Add an event to call onDidChangeDate function when value is changed.
+        datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged(_:)), for: .valueChanged)
+    }
 
     @IBAction func updateSelectedDateField(sender: DatePicker) {
     DatePicked.text = DatePicker
@@ -34,3 +40,17 @@ func ConfigureDatePicker(dPicker: UIDatePicker) {
     dPicker.backgroundColor = UIColor.white
     
 } 
+
+func datePickerValueChanged(_ sender: UIDatePicker){
+
+    // Create date formatter
+    let dateFormatter: DateFormatter = DateFormatter()
+
+    // Set date format
+    dateFormatter.dateFormat = "MM/dd/yyyy"
+
+    // Apply date format
+    let selectedDate: String = dateFormatter.string(from: sender.date)
+
+    print("Selected value \(selectedDate)")
+}
