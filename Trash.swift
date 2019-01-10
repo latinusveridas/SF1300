b18ef0867b70d5f3bbca6d1b89aa644a2498b49d
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-10 14:05:09 +0100

    Update Trash.swift

--- Trash.swift
+++ Trash.swift
@@ -1,48 +1,16 @@
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        // Create a DatePicker
-        let datePicker: UIDatePicker = UIDatePicker()
-        
-        // Posiiton date picket within a view
-        datePicker.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 200)
-        
-        // Set some of UIDatePicker properties
-        datePicker.timeZone = NSTimeZone.local
-        datePicker.backgroundColor = UIColor.white
-        
-        // Add an event to call onDidChangeDate function when value is changed.
-        datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged(_:)), for: .valueChanged)
-   
-        // Add DataPicker to the view
-        self.view.addSubview(datePicker)
-        
-    }
-    
-    
-    func datePickerValueChanged(_ sender: UIDatePicker){
-        
-        // Create date formatter
-        let dateFormatter: DateFormatter = DateFormatter()
-        
-        // Set date format
-        dateFormatter.dateFormat = "MM/dd/yyyy hh:mm a"
-        
-        // Apply date format
-        let selectedDate: String = dateFormatter.string(from: sender.date)
-        
-        print("Selected value \(selectedDate)")
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-}
+// need async + network connection
+// On recupere les coordinates de ? et on on fait un reverse GeoCoding
+
+/*dictionary GeocoderReverseLookupOptions {
+    String language;
+}; */
+
+let optionsLookup: GeocoderReverseLookupOptions 
+optionsLookup.language = "FR-fr"
+
+// Create Object
+let geoCoder = new mapkit.Geocoder(optional GeocoderConstructorOptions options);
